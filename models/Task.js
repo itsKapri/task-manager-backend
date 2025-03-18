19e3915a85a28c86f9a5fd5957d80a159acdc4ef
नीलेश <70235074+itsKapri@users.noreply.github.com>
2025-03-18 20:29:40 +0000

    fixed the TaskSchema

--- models/Task.js
+++ models/Task.js
@@ -4,7 +4,7 @@ const TaskSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   title: String,
   description: String,
-  createdAt: Date.now
+  createdAt: { type: Date, default: Date.now }
 }); 
 
 module.exports = mongoose.model('Task', TaskSchema);
