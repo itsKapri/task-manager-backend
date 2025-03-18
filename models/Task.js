748ae717528babe37933709424708fbaa4049c96
नीलेश <70235074+itsKapri@users.noreply.github.com>
2025-03-18 20:00:14 +0000

    Minor fix

--- models/Task.js
+++ models/Task.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   title: String,
-  description: String
-}, { timestamps: true }); 
+  description: String,
+  createdAt: Date.now
+}); 
 
 module.exports = mongoose.model('Task', TaskSchema);
